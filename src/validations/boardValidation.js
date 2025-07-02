062416a5fa167102c537523e410ebaea7d74cb6e
Tuans09 <dt2003911@gmail.com>
2025-07-02 16:36:07 +0700

    controller_handling

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from "joi";
 import { StatusCodes } from "http-status-codes";
+import ApiError from "~/utils/ApiError";
 
 const createNew = async (req, res, next) => {
     const correctCondition = Joi.object({
@@ -15,14 +16,14 @@ const createNew = async (req, res, next) => {
 
     try {
         await correctCondition.validateAsync(req.body, { abortEarly: false });
-        //next()
-        res.status(StatusCodes.CREATED).json({
-            message: "Board created successfully",
-        });
+        next();
     } catch (error) {
-        res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-            errors: new Error(error).message,
-        });
+        const errorMessages = new Error(error).message;
+        const customError = new ApiError(
+            StatusCodes.UNPROCESSABLE_ENTITY,
+            errorMessages
+        );
+        next(customError);
     }
 };
 
