2bbd368758f8d765434cf27623cdb4a9db219892
Tuans09 <dt2003911@gmail.com>
2025-07-02 18:04:37 +0700

    service_layer

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,13 +1,15 @@
 import { StatusCodes } from "http-status-codes";
 import ApiError from "~/utils/ApiError";
+import { boardService } from "~/services/boardService";
 const createNew = async (req, res, next) => {
     try {
         // throw new ApiError(
         //     StatusCodes.BAD_GATEWAY,
         //     "This is a test error from the boardController"
         // );
+        const createdBoard = await boardService.createNew(req.body);
         res.status(StatusCodes.CREATED).json({
-            message: "POST from controller: API create new board",
+            createdBoard,
         });
     } catch (error) {
         next(error);
---
+++ src/services/boardService.js
@@ -0,0 +1,17 @@
+import { slugify } from "~/utils/formatters";
+const createNew = async (reqBody) => {
+    try {
+        const newBoard = {
+            ...reqBody,
+            slug: slugify(reqBody.title),
+        };
+        return newBoard;
+    } catch (error) {
+        {
+            throw error;
+        }
+    }
+};
+export const boardService = {
+    createNew,
+};
