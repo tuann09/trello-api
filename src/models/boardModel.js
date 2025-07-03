bd9eb4a26ebae0efeb0e10b35a44970834ded5a6
Tuans09 <dt2003911@gmail.com>
2025-07-03 19:35:36 +0700

    aggregate query join du lieu column

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -3,13 +3,18 @@ import Joi from "joi";
 import { GET_DB } from "~/config/mongodb";
 import { ObjectId } from "mongodb";
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators";
-
+import { BOARD_TYPES } from "~/utils/constants";
+import { columnModel } from "./columnModel";
+import { cardModel } from "./cardModel";
 //Define collection
 const BOARD_COLLECTION_NAME = "boards ";
 const BOARD_COLLECTION_SCHEMA = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict(),
     slug: Joi.string().required().min(3).trim().strict(),
     description: Joi.string().required().min(3).max(255).trim().strict(),
+    type: Joi.string()
+        .valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE)
+        .required(),
     columnOrderIds: Joi.array()
         .items(
             Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
@@ -49,10 +54,33 @@ const findOneById = async (id) => {
 };
 const getDetails = async (id) => {
     try {
+        // const result = await GET_DB()
+        //     .collection(BOARD_COLLECTION_NAME)
+        //     .findOne({ _id: new ObjectId(id) });
+        // return result;
         const result = await GET_DB()
             .collection(BOARD_COLLECTION_NAME)
-            .findOne({ _id: new ObjectId(id) });
-        return result;
+            .aggregate([
+                { $match: { _id: new ObjectId(id), _destroy: false } },
+                {
+                    $lookup: {
+                        from: columnModel.COLUMN_COLLECTION_NAME,
+                        localField: "_id",
+                        foreignField: "boardId",
+                        as: "columns",
+                    },
+                },
+                {
+                    $lookup: {
+                        from: cardModel.CARD_COLLECTION_NAME,
+                        localField: "_id",
+                        foreignField: "boardId",
+                        as: "cards",
+                    },
+                },
+            ])
+            .toArray();
+        return result[0] || {};
     } catch (error) {
         throw new Error(error);
     }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -2,6 +2,7 @@ import { slugify } from "~/utils/formatters";
 import { boardModel } from "~/models/boardModel";
 import ApiError from "~/utils/ApiError";
 import { StatusCodes } from "http-status-codes";
+import { cloneDeep } from "lodash";
 const createNew = async (reqBody) => {
     try {
         const newBoard = {
@@ -25,7 +26,18 @@ const getDetails = async (boardId) => {
         if (!board) {
             throw new ApiError(StatusCodes.NOT_FOUND, "Board not found");
         }
-        return board;
+        const resBoard = cloneDeep(board);
+        resBoard.columns.forEach((column) => {
+            column.cards = resBoard.cards.filter((card) =>
+                card.columnId.equals(column._id)
+            );
+            column.cards = resBoard.cards.filter(
+                (card) => card.columnId.toString() === column._id.toString()
+            );
+        });
+        delete resBoard.cards;
+
+        return resBoard;
     } catch (error) {
         {
             throw error;
