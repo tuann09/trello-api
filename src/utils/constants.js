51b094fc7e74dfb5721a612e2c852205cf31ae05
Tuans09 <dt2003911@gmail.com>
2025-07-10 23:22:32 +0700

    update again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const WHITELIST_DOMAINS = ["https://trello-vl7o.vercel.app/"];
+export const WHITELIST_DOMAINS = ["https://trello-vl7o.vercel.app"];
 export const BOARD_TYPES = {
     PUBLIC: "public",
     PRIVATE: "private",
