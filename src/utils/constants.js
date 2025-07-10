53477b42376d9b8c5194529bde9d3a792963cdcd
Tuans09 <dt2003911@gmail.com>
2025-07-10 23:20:27 +0700

    upadte whitelist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
-export const WHITELIST_DOMAINS = [
-    // "http://localhost:5173"
-];
+export const WHITELIST_DOMAINS = ["https://trello-vl7o.vercel.app/"];
 export const BOARD_TYPES = {
     PUBLIC: "public",
     PRIVATE: "private",
